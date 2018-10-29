769fefc6aac7bc99cdd1f0959e45b50a4e732b40
Family Laptop <Extra@metall.local>
2018-10-29 23:29:13 +0200

    poker layers

--- pocker.py
+++ pocker.py
@@ -5,8 +5,17 @@ from keras.utils import np_utils
 import numpy as np
 
 
-
+X_train = []
+y_train = []
 np.random.seed(42)
 X_train = X_train.reshape(60000, 784)
 
 Y_train = np_utils.to_categorical(y_train, 10)
+
+
+model = Sequential()
+
+model.add(Dense(85, input_dim=784, activation="relu", kernel_initializer="normal"))
+model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
+
+model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
