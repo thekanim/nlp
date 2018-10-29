73b0ba917288b720046126205ae4c6aad43ed110
Family Laptop <Extra@metall.local>
2018-10-29 23:17:14 +0200

    ini poker hand recognizer

--- pocker.py
+++ pocker.py
@@ -1,6 +1,12 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import SGD
+from keras.utils import np_utils
 import numpy as np
 
 
+
+np.random.seed(42)
+X_train = X_train.reshape(60000, 784)
+
+Y_train = np_utils.to_categorical(y_train, 10)
